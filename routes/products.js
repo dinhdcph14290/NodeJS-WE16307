5f1cdaac7e586843f59ddd86489a6c76d4c7da36
CongDinh <dinhdcph14290@fpt.edu.vn>
2022-03-17 13:47:23 +0700

    update

--- routes/products.js
+++ routes/products.js
@@ -4,18 +4,22 @@ import { request } from "express";
 const router = express.Router();
 const products = [
     {id:1,name:"Product 1"},
-    {id:2,name:"Product 2"}
+    {id:2,name:"Product 2"},
+    {id:3,name:"Product 3"}
 ]
+
+// show products
 router.get('/products',(request,response)=>{
     response.json(products)
 });
+// Show detail 
 router.get('/products/:id',(request,response)=>{
     const product = products.find(item => item.id === +request.params.id)
     response.json(product)
 });
 router.post('/products',(request,response)=>{
     products.push(request.body)
-    response.json(product)
+    response.json(products)
 });
 router.delete('/products:id',(request,response) =>{
     const product = products.filter(item.id != +request.params.id)
