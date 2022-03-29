2874e573bbc136263752bc524821e424f696ef1d
CongDinh <dinhdcph14290@fpt.edu.vn>
2022-03-29 17:54:14 +0700

    feat: signup

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import morgan from 'morgan';
+
 import homeRoute from './routes/home'
 import productRoute from './routes/products'
 import categoryRoute from './routes/category'
+import authRoute from './routes/auth'
 const app = express();
 
-
-app.use(morgan('tiny'))
-app.use(express.json())  // to read json data when uploaded
-
+app.use(express.json())
 app.use(homeRoute);
 app.use("/api",productRoute);
-app.use("/api",categoryRoute);
+app.use("/api",categoryRoute)
+app.use("/api",authRoute)
 
 mongoose.connect('mongodb://localhost:27017/we16307');
 
---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+import User from '../models/user'
+
+export const signup = async (request,response)=>{
+    const {email,name,password} = request.body
+    console.log(request.body)
+    try {
+        const existUser = await User.findOne({email}).exec()
+        if(existUser){
+            return response.status(400).json({
+                message:"User ton tai"
+            })
+        }
+        const user = await User({email,name,password}).save()
+        response.json({
+            user:{
+                _id:user._id,
+                email:user.email,
+                name:user.name
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
