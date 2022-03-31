8d72d738e8548ba99e9301cf5c5e5ffe92ca3818
CongDinh <dinhdcph14290@fpt.edu.vn>
2022-03-31 18:02:59 +0700

    feat: createProduct

---
+++ controllers/user.js
@@ -0,0 +1,18 @@
+// Su dung de tim kiem
+import User from '../models/user'
+
+export const userById = async (request, response, next, id) => {
+    try {
+        const user = await User.findById(id).exec();
+        if(!user){
+            response.status(400).json({
+                message: "Không tìm thấy user"
+            })
+        }
+        request.profile = user;
+        request.profile.password = undefined;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,15 @@ import express from "express";
 import { response } from "express";
 import { request } from "express";
 import { createProduct, deleteProduct, listProduct, listProductDetail, updateProduct } from "../controllers/products";
+import { userById } from "../controllers/user";
 const router = express.Router();
 
 router.get('/products',listProduct);
 router.get('/products/:id',listProductDetail);
-router.post('/products',createProduct);
+router.post('/products/:userId',createProduct);
 router.delete('/products:id', deleteProduct)
 router.put('/products/:id',updateProduct)
+
+
+router.param("userId",userById)
 export default router;
